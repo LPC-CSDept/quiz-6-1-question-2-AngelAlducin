7dfd8ef3522b871f23b5d062e87c37cc757f341e
AngelAlducin <aualducin@gamil.com>
2022-10-13 16:03:15 -0700

    setup

--- q2.cpp
+++ q2.cpp
@@ -2,12 +2,21 @@
 #include <iomanip>
 using namespace std;
 
-// ******************************
-// Your code here
-// ******************************
 
-// ******************************
-// this function will help you pass the test. When you print the result with this function, you will get the same output format
+void getInput(int &n1, int &n2, int &n3);
+
+int findMin(int n1, int n2, int n3);
+
+void printResult(int n1, int n2, int n3, int min);
+
+int main()
+{
+	int n1, n2, n3, min;
+	getInput(n1, n2, n3);
+	min = findmin(n1, n2, n3);
+	printResult(n1, n2, n3, min);
+}
+
 void printResult(int n1, int n2, int n3, int min)
 {
 	cout << n1 << " " << n2 << " " << n3 << " " << min << " " << endl;
