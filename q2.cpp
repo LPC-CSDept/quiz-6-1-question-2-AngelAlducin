357bb87f32bb06dba73d9f49289a5d66ecab0d77
AngelAlducin <aualducin@gamil.com>
2022-10-13 16:11:43 -0700

    last two fuction

--- q2.cpp
+++ q2.cpp
@@ -13,8 +13,24 @@ int main()
 {
 	int n1, n2, n3, min;
 	getInput(n1, n2, n3);
-	min = findmin(n1, n2, n3);
+	min = findMin(n1, n2, n3);
 	printResult(n1, n2, n3, min);
+}
+
+void getInput(int &n1, int &n2, int &n3)
+{
+cout << "Enter thre integers: ";
+cin >> n1 >> n2 >> n3;
+}
+
+int findMin(int n1, int n2, int n3)
+{
+	if(n1 < n2 && n1 < n3)
+		return n1;
+	else if(n2 < n3 && n2 < n1)
+		return n2;
+	else
+		return n3;
 }
 
 void printResult(int n1, int n2, int n3, int min)
